656488ae22eeba2b7d508d4bd86543f22cbde5cc
Andrew Cutler <andrewkcutler@gmail.com>
2022-05-09 18:20:04 -0400

    save entire result display to local storage

--- src/TextDisplay.tsx
+++ src/TextDisplay.tsx
@@ -1,8 +1,14 @@
-import { chakra } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import ReactDOMServer from 'react-dom/server';
 
 const green = '#48bb78';
 const red = '#f56565';
+
+export interface IStorageValue {
+	rendered: string;
+	text: string;
+	accuracy: number;
+}
 
 const TextDisplay = ({
 	text,
@@ -16,29 +22,41 @@ const TextDisplay = ({
 	index: number;
 	storageKey: string;
 	completed: boolean;
-}): React.ReactElement => (
-	<>
-		{text.slice(0, index).map((word, index) => {
+}): React.ReactElement => {
+	const [displayText, setDisplayText] = useState<JSX.Element[]>([]);
+
+	useEffect(() => {
+		if (completed) {
+			const final = <div>{[...displayText]}</div>;
+			const finalAsString = ReactDOMServer.renderToStaticMarkup(final);
+			localStorage.setItem(
+				storageKey,
+				JSON.stringify({
+					rendered: finalAsString,
+					text: 'TEST',
+					accuracy: 75
+				})
+			);
+		}
+	}, [completed, displayText, storageKey]);
+
+	useEffect(() => {
+		const result = text.slice(0, index).map((word, index) => {
 			const key = `${word}_${index}`;
 
 			// cannot use chakra styling syntax/variables as it compiles to stuff like class="css-1ou2t5u"
 			const displayText = (
-				<chakra.span
-					style={{ color: guesses[index] ? green : red }}
-					key={key}
-				>
+				<span style={{ color: guesses[index] ? green : red }} key={key}>
 					{word + ' '}
-				</chakra.span>
+				</span>
 			);
 
-			if (completed) {
-				console.log(ReactDOMServer.renderToStaticMarkup(displayText));
-				localStorage.setItem(storageKey, displayText.toString());
-			}
-
 			return displayText;
-		})}
-	</>
-);
+		});
+		setDisplayText(result);
+	}, [guesses, index, storageKey, text]);
+
+	return <>{[...displayText]}</>;
+};
 
 export default TextDisplay;
